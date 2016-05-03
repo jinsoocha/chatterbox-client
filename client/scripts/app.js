f4c8918175fe20262197697d84d816e020d8b127
jinsoocha <jinscha@indiana.edu>
2016-05-02 19:08:45 -0700

    added escaping for message and username

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,41 +1,56 @@
 // YOUR CODE HERE:
 
 //https://api.parse.com/1/classes/messages
-let app = {
-  server: 'https://api.parse.com/1/classes/messages',
-  init: function() {
-    $('#send').submit(this.handleSubmit);
-  },
-  send: function(message) {
-    $.ajax({
-      url: this.server,
-      type: 'POST',
-      data: JSON.stringify(message)
-    });
-  },
-  fetch: function() {
-    $.ajax({
-      url: this.server,
-      type: 'GET'
-    });
-  },
-  clearMessages: function() {
-    $('#chats').html('');
-  },
-  addMessage: function(message) {
-    let thisMessage = $(`<div><div class="username">${message.username}</div>` +
-      `<div class="message">${message.text}</div></div>`);
-    $('#chats').append(thisMessage);
-    thisMessage.find('.username').click(() => this.addFriend(message.username));
-  },
-  addRoom: function(roomname) {
-    $('#roomSelect').append(`<div>${roomname}</div>`);
-  },
-  addFriend: function(username) {
-    console.log('added Friend: ' + username);
-  },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    console.log('Submit');
-  }
-};
+
+
+$(document).ready(function() {
+  let app = {
+    server: 'https://api.parse.com/1/classes/messages',
+    init: function() {
+      $('#send').submit(this.handleSubmit);
+    },
+    send: function(message) {
+      $.ajax({
+        url: this.server,
+        type: 'POST',
+        data: JSON.stringify(message)
+      });
+    },
+    fetch: function() {
+      $.ajax({
+        url: this.server,
+        type: 'GET',
+        contentType: 'application/json',
+        success: (data) => this.completedFetch(data),
+        error: (data) => console.error('chatterbox: Failed to fetch messages', data)
+      });
+    },
+    clearMessages: function() {
+      $('#chats').html('');
+    },
+    addMessage: function(message) {
+      let thisMessage = $(`<div class="message-box"><div class="username">${_.escape(message.username)}</div>` +
+        `<div class="message">${_.escape(message.text)}</div></div>`);
+      $('#chats').append(thisMessage);
+      thisMessage.find('.username').click(() => this.addFriend(message.username));
+    },
+    addRoom: function(roomname) {
+      $('#roomSelect').append(`<div>${_.escape(roomname)}</div>`);
+    },
+    addFriend: function(username) {
+      console.log('added Friend: ' + username);
+    },
+    handleSubmit: function(e) {
+      e.preventDefault();
+      console.log('Submitting', $('#message').val());
+    },
+    completedFetch(message) {
+      for (let result of message.results) {
+        this.addMessage(result);
+        console.log(result.text);
+      }
+    }
+  };
+  app.init();
+  app.fetch();
+});
